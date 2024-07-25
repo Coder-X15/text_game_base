4c399d6d620b8b6b26b351ff5eecc35243f01766
Coder-X15 <samrubenabraham@gmail.com>
2024-07-25 23:47:28 +0530

    Added new files

---
+++ src/scene.hpp
@@ -0,0 +1,26 @@
+# pragma once
+
+# ifndef SCENE_H
+# define SCENE_H
+
+# include <iostream>
+
+namespace scene_util{
+    class Character{
+        // abstract class to make the character from
+        public:
+            virtual bool isTurn() = 0; // just set up this function so as to enable fight mode as well in the games
+            virtual int behave() = 0; // the function to invoke the character's behaviour into the game
+    };
+
+    class Scene{
+        // abstract class to inherit the scene architecture from
+        public:
+            virtual int next_scene() = 0; // returns the integer index of the next scene in the scene array
+            virtual int run() = 0; // runs the scene
+            virtual bool achieved_objective() = 0; // checks if the objective in the scene was achieved
+            virtual bool is_locked() = 0; // checks if the scene is locked
+    };
+}
+
+# endif
